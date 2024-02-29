f4f134bb6538f193df732e59e27193038dcde4ec
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-02-29 13:51:38 -0500

    primera información completa 1ra categoria

--- Catálogo.py
+++ Catálogo.py
@@ -9,10 +9,12 @@
 menu=dict({
 
 #categorías(En Python, cuando defines un diccionario, cada par clave-valor debe estar separado por una coma ,)
+#dentro de la categoria asignaremos una variable de producto como lista
 
 #ahora agregamos la info dentro de estas para tener pares de Clave-Valor
+#Dentro de las claves-valor irá la información del nombre del producto-precio del producto.(números no llevan comillas doblres por ser valores numéricos)
 #CERO
-    "Dulces":{"nombre": "", "valor":""},
+    "Dulces": {"producto": list([{"nombre":"Croissants", "valor":3500},{"nombre": "Donas", "valor":2000},{"nombre": "Pastelitos", "valor":1500},{"nombre":"Galletas", "valor":7000},{"nombre":"Pan dulce","valor":5000},{"nombre":"Ensaimadas", "valor":6000},{"nombre":"Tartaletas", "valor":4500},{"nombre":"Bizcochos", "valor":2500},{"nombre":"Palmeras", "valor":2000},{"nombre":"Conchas", "valor":2000}])},
 #UNO
     "Salados":{"nombre": "", "valor":""},
 #DOS
