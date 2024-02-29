a8b91c231b1423ef1c5635bd6b518a6c8bd43e6e
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-02-29 13:16:16 -0500

    Orden diccionario

---
+++ Catálogo.py
@@ -0,0 +1,19 @@
+
+
+#crear nuestra información completa y detallada, en este caso 3 categorías con 3 promociones cada una
+#crear código que muestre, deje seleccionar, muestre valor, deje contar cuanto dinero, de vueltos y deje comprar promociones
+
+
+
+#Diccionario: bloque que retendrá toda nuestra información
+menu=dict({
+
+#categorías
+
+#CERO
+    Dulces=
+#UNO
+    Salados=
+#DOS
+    Pasteles=
+})
