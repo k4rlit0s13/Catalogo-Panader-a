18f7fae16589e42d3a99fef164c51593ed184e3e
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-02-29 16:01:33 -0500

    categorias completadas

--- Catálogo.py
+++ Catálogo.py
@@ -13,10 +13,11 @@ menu=dict({
 
 #ahora agregamos la info dentro de estas para tener pares de Clave-Valor
 #Dentro de las claves-valor irá la información del nombre del producto-precio del producto.(números no llevan comillas doblres por ser valores numéricos)
-#CERO
-    "Dulces": {"producto": list([{"nombre":"Croissants", "valor":3500},{"nombre": "Donas", "valor":2000},{"nombre": "Pastelitos", "valor":1500},{"nombre":"Galletas", "valor":7000},{"nombre":"Pan dulce","valor":5000},{"nombre":"Ensaimadas", "valor":6000},{"nombre":"Tartaletas", "valor":4500},{"nombre":"Bizcochos", "valor":2500},{"nombre":"Palmeras", "valor":2000},{"nombre":"Conchas", "valor":2000}])},
-#UNO
-    "Salados": {"producto": list([{"nombre":"Baguettes", "valor":5000},{"nombre":"Pan de masa madre","valor":6000},{"nombre":"Pan de ajo","valor":4000},{"nombre":"Ocaccia","valor":8000},{"nombre":"Pretzels","valor":4500},{"nombre":"Bol de queso","valor":2500},{"nombre":"Empanadas","valor":2000},{"nombre":"Pan de centeno","valor":5500},{"nombre":"Pan de aceitunas","valor":4500},{"nombre":"Pan de hierbas","valor":4000},]),
-    "Pasteles":{"nombre": "", "valor":""}, 
+#Categoria CERO
+    "Dulces":  {"producto": list([{"nombre":"Croissants", "valor":3500},{"nombre": "Donas", "valor":2000},{"nombre": "Pastelitos", "valor":1500},{"nombre":"Galletas", "valor":7000},{"nombre":"Pan dulce","valor":5000},{"nombre":"Ensaimadas", "valor":6000},{"nombre":"Tartaletas", "valor":4500},{"nombre":"Bizcochos", "valor":2500},{"nombre":"Palmeras", "valor":2000},{"nombre":"Conchas", "valor":2000}])},
+#categoria UNO
+    "Salados": {"producto": list([{"nombre":"Baguettes", "valor":5000},{"nombre":"Pan de masa madre","valor":6000},{"nombre":"Pan de ajo","valor":4000},{"nombre":"Ocaccia","valor":8000},{"nombre":"Pretzels","valor":4500},{"nombre":"Bol de queso","valor":2500},{"nombre":"Empanadas","valor":2000},{"nombre":"Pan de centeno","valor":5500},{"nombre":"Pan de aceitunas","valor":4500},{"nombre":"Pan de hierbas","valor":4000}])},
+#categoria DOS   
+    "Pasteles":{"producto": List([{"nombre": "Torta de chocolate", "valor":40000},{"nombre":"Tarta de manzana","valor":35000},{"nombre":"Torta de zanahoria","valor":45000},{"nombre":"Tarta red Velvet","valor":40000},{"nombre":"Tarta de limón","valor":50000},{"nombre":"Torta de queso","valor":45000},{"nombre":"Torta de Frutas","valor":50000},{"nombre":"Tarta Sacher","valor":55000},{"nombre":"Torta 3 leches","valor":40000},{"nombre":"Torta de nueces y caramelo","valor":50000}])}
     
 })
